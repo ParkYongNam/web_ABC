523c26c644a530ed38a5cbf548831d9165282fbb
ParkYongNam <dkskcka3@naver.com>
2023-11-03 18:13:18 +0900

    A형 상하스와이퍼 완성

--- js/web.js
+++ js/web.js
@@ -1,4 +1,25 @@
 $(document).ready(function () {
     //화면이 준비되면 실행해라.
 
+    setInterval(function () {
+        $(".swiper-wrapper").animate({
+            "marginLeft": -1200
+        }, 400, function () {
+            $("div:first-child", this).appendTo($(this))
+            $(this).css("marginLeft", 0)
+        })
+    }, 3000)
 })
+//$(document).ready(function () {
+//화면이 준비되면 실행해라.
+
+//    setInterval(function () {
+//        $(".swiper-wrapper").animate({
+//            "marginLeft": -300
+//        }, 400, function () {
+//            $("div:first-child", this).appendTo($(this))
+//            $(this).css("marginLeft", 0)
+//        })
+//    }, 3000)
+//})     d-felx를 풀면 위로 올라간다
+
